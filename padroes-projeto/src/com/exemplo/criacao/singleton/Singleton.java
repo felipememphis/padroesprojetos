4b512bc6d0ec41414d06a70ee7c8184c59fc8c22
Felipe Lemos <felipememphis@hotmail.com>
2018-03-10 10:55:10 -0300

    padrão de criação singleton

--- padroes-projeto/src/com/exemplo/criacao/singleton/Singleton.java
+++ padroes-projeto/src/com/exemplo/criacao/singleton/Singleton.java
@@ -4,8 +4,7 @@ public class Singleton {
 
 	private static final Singleton INSTANCE = new Singleton();
 
-	private Singleton() {
-	}
+	private Singleton() {}
 
 	public static final Singleton getInstance() {
 		return INSTANCE;
